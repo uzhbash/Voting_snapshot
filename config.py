8295170e65234228f6b05971ecb41985d2e0dbd8
uzbash <134600527+uzhbash@users.noreply.github.com>
2024-01-27 21:52:55 +0300

    Update config.py
    added vote choices randomising

--- config.py
+++ config.py
@@ -2,3 +2,5 @@ TIME = 30
 TIMEMAX = 3000
 USE_PROXY = False
 TIME_ERROR = 10 # sec await after error
+VOTE_CHOICES = [1, 2, 3]
+RANDOM_CHOICES = True # True - if you want to your accs to choose randomly from [YES, NO, ABSTAIN]
